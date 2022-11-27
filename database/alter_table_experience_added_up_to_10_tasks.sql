dd0a60f8a35a86444b2773b1dd1fa876106f337b
Cristina <andreea.luta@yahoo.ro>
2022-11-27 14:22:45 +0200

    Added the possibility to include up to 10 tasks in the CV. But only 3 of them are mandatory

---
+++ database/alter_table_experience_added_up_to_10_tasks.sql
@@ -0,0 +1,9 @@
+ALTER TABLE portfolio_db.experience ADD task_2 varchar(100) NOT NULL;
+ALTER TABLE portfolio_db.experience ADD task_3 varchar(100) NOT NULL;
+ALTER TABLE portfolio_db.experience ADD task_4 varchar(100) NULL;
+ALTER TABLE portfolio_db.experience ADD task_5 varchar(100) NULL;
+ALTER TABLE portfolio_db.experience ADD task_6 varchar(100) NULL;
+ALTER TABLE portfolio_db.experience ADD task_7 varchar(100) NULL;
+ALTER TABLE portfolio_db.experience ADD task_8 varchar(100) NULL;
+ALTER TABLE portfolio_db.experience ADD task_9 varchar(100) NULL;
+ALTER TABLE portfolio_db.experience ADD task_10 varchar(100) NULL;
